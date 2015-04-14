4c67038175298d2d3f71ff5a25866cebfd4d7da4
SwarShah <swar12894@gmail.com>
2015-04-14 19:54:00 -0400

    methods to return JSON

--- src/java/serverPackage/JSONHelper.java
+++ src/java/serverPackage/JSONHelper.java
@@ -11,5 +11,22 @@ package serverPackage;
  * @author c0647456
  */
 public class JSONHelper {
+    public String getNewClientDetailsJson(String sessionId, String message) {
+        return null;        
+    }
     
+    public String getConnectJson(String sessionId, String name,
+			String message, int onlineCount) {
+        return null;        
+    }
+    
+    public String getDisconnectJson(String sessionId, String name,
+			String message, int onlineCount) {
+        return null;        
+    }
+    
+    public String getMessageJson(String sessionId, String fromName,
+			String message) {
+        return null;        
+    }
 }
