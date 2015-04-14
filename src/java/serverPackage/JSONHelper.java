52c428e2e8ae1917297c1f7252f4ce002613efe1
SwarShah <swar12894@gmail.com>
2015-04-14 19:49:24 -0400

    Created JSONHelper class to return JSON string

---
+++ src/java/serverPackage/JSONHelper.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package serverPackage;
+
+/**
+ *
+ * @author c0647456
+ */
+public class JSONHelper {
+    
+}
