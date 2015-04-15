84e07317388d47b471376dcf411dc8185967ed4c
SwarShah <swar12894@gmail.com>
2015-04-14 20:15:33 -0400

    method for sending msg accordingly

--- src/java/serverPackage/SocketServer.java
+++ src/java/serverPackage/SocketServer.java
@@ -6,6 +6,7 @@
 
 package serverPackage;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -26,6 +27,7 @@ public class SocketServer {
     private static final Set<Session> liveSessions = Collections.synchronizedSet(new HashSet<Session>());
     private static final HashMap<String, String> nameWithSession = new HashMap<>();
     private JSONHelper json = new JSONHelper();
+    
     @OnMessage
     public String onMessage(String message, Session s) {
         System.out.println("Message from: "+s.getId() +", Message: "+message);
@@ -52,5 +54,29 @@ public class SocketServer {
         System.out.println("Sending Msg: "+msg);
         s.getAsyncRemote().sendText(msg);
     }
+    
+    private void sendMessageToAll(String sessionId, String name,
+			String message, boolean isNewClient, boolean isExit) {
+		for (Session s : liveSessions) {
+			String jsonString = "";
+			if (isNewClient) {
+				jsonString = json.getConnectJson(sessionId, name, message,
+						liveSessions.size());
+
+			} else if (isExit) {
+				jsonString = json.getDisconnectJson(sessionId, name, message,
+						liveSessions.size());
+			} else {
+				jsonString = json
+						.getMessageJson(sessionId, name, message);
+			}
+                        try {				
+				s.getBasicRemote().sendText(jsonString);
+			} catch (IOException e) {
+				System.out.println("Error");
+				e.printStackTrace();
+			}
+		}
+	}
     
 }
